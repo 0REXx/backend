844c94eed83e3a946d35c411039b5e86614fe42c
0REXx <166982046+0REXx@users.noreply.github.com>
2024-12-03 02:48:30 +0100

    Update shoppingListRoutes.js

--- backend/routes/shoppingListRoutes.js
+++ backend/routes/shoppingListRoutes.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const { getShoppingLists, createShoppingList, updateShoppingList, deleteShoppingList } = require('../controllers/shoppingListController');
